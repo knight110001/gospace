0cad2895fec9bbf2f2dd8e60da09d26bb9192721
knight110001 <hy110001@gmail.com>
2020-01-17 22:48:21 +0800

    add on golang script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gospace
 DevOps scripts for golang setup
+
+### :sparkles: Description ###
+GoLang Compile Environment
+
+
+### :rocket: Installation ###
+
+```
+1. git clone https://github.com/knight110001/gospace.git
+2. ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PKG=go1.13.6
+
+# download go package
+if [ -f $PKG.linux-amd64.tar.gz ]; then
+  wget -c https://dl.google.com/go/$PKG.linux-amd64.tar.gz
+  tar -xzf $PKG.linux-amd64.tar.gz
+  sudo mv go /opt/$PKG
+
+  if [ ! -L /opt/go  ]; then
+    sudo ln -sf /opt/$PKG /opt/go
+  fi
+fi
+
+# prepare setup script
+if [ ! -f $PKG/setup.sh ]; then
+
+cat <<EOF > $PKG/setup.sh
+#!/bin/bash
+export GOPROXY=https://goproxy.cn
+export GOROOT=/opt/go
+export GOPATH=\$HOME/gows
+export PATH=\$GOROOT/bin:\$GOPATH/bin:\$PATH
+EOF
+
+  chmod +x $PKG/setup.sh
+
+fi
+
+# append on bashrc
+num=`grep -e "/opt/go/setup.sh" ~/.bashrc | wc -l`
+if [ $num != "0" ]; then
+
+cat <<EOF >> ~/.bashrc
+
+if [ -f /opt/go/setup.sh ]; then
+    source /opt/go/setup.sh
+fi
+
+EOF
+
+fi
+
