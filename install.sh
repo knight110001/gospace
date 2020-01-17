4dff4da01e8f70ee7772d3956ee1a1b800f4cd42
knight110001 <hy110001@gmail.com>
2020-01-17 23:22:03 +0800

    bugfix for zip files check

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 PKG=go1.13.6
 
 # download go package
-if [ -f $PKG.linux-amd64.tar.gz ]; then
+if [ ! -f $PKG.linux-amd64.tar.gz ]; then
   wget -c https://dl.google.com/go/$PKG.linux-amd64.tar.gz
   tar -xzf $PKG.linux-amd64.tar.gz
   sudo mv go /opt/$PKG
@@ -14,9 +14,9 @@ if [ -f $PKG.linux-amd64.tar.gz ]; then
 fi
 
 # prepare setup script
-if [ ! -f $PKG/setup.sh ]; then
+if [ ! -f /opt/$PKG/setup.sh ]; then
 
-cat <<EOF > $PKG/setup.sh
+cat <<EOF > setup.sh
 #!/bin/bash
 export GOPROXY=https://goproxy.cn
 export GOROOT=/opt/go
@@ -24,13 +24,14 @@ export GOPATH=\$HOME/gows
 export PATH=\$GOROOT/bin:\$GOPATH/bin:\$PATH
 EOF
 
-  chmod +x $PKG/setup.sh
+  chmod +x setup.sh
+  sudo mv setup.sh /opt/$PKG/
 
 fi
 
 # append on bashrc
 num=`grep -e "/opt/go/setup.sh" ~/.bashrc | wc -l`
-if [ $num != "0" ]; then
+if [ $num == "0" ]; then
 
 cat <<EOF >> ~/.bashrc
 
